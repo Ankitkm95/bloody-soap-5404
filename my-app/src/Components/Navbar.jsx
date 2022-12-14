ef0b228be1bc25ffbd9b08bc3aaec4dc5feffb83
Ankitkm95 <ankitmishra517@gmail.com>
2022-12-14 23:03:09 +0530

    day_2Navbar

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+
+const Navbar = () =>{
+    return(
+        <div className="main">
+            <h1>tripoto</h1>
+            <div className="sub1">
+                <input placeholder='Search for itineraries, destinations, hotels or activities'></input>
+            </div>
+            <div className="sub2">
+            <h3>Inspirations</h3>
+            <h3>Forum</h3>
+            <h3>Packages</h3>
+            <h3>Publish</h3>
+            <h3>Sign in</h3>
+            </div>
+            
+        </div>
+    );
+}
+export default Navbar;
